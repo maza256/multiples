b8adcf6802bcab53db595bd361c550e81357dd11
Marek Stefanowski <>
2018-03-02 10:42:14 +0000

    Version 1.0 of Multiples API

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# multiples
-API to test if a request contains a number that is a multiple of 7 and/or 9
+## Multiples Test API
+
+This is an API implemented in the Go Language developed by Google, which reads in from a GET request a value and returns a JSON format file whether the number is a multiple of 7 and/or 9.
+
+
+Due to time constraints error handling and code re-use have not been a priority.
+
+# Build and Run
+
+To run the server an Installation of Go lang must be present (v1.6.2+) and the path to the install location added to the path environment variable. 
+
+Once installed, the following command must be issued to install a required package:
+`go get github.com/gorilla/mux`
+
+Once installed the code can be compiled with:
+`go build multiple_API.go`
+
+The compiled executable can now be run to start the server. This may need to be run as a root due to using port 80, which some systems may protect from use.
+The code may be modified to utilise a different port number to solve this. If changed, the browser call will need to include the port number used in the following manner, example port number 8080:
+`localhost:8080/GET/<number_to_be_evaluated>`
---
+++ multiples_API.go
@@ -0,0 +1,104 @@
+/*
+/   Author: Marek Stefanowski
+/   Description: A Golang server implementation to identify if a number is a multiple of 7 and/or 9
+/   Licence: Free
+*/
+package main
+
+// Imports required to carry out the functionality
+import (
+    "encoding/json"
+    "fmt"
+    "net/http"
+    "strconv"
+    "time"
+    "github.com/gorilla/mux"
+)
+
+// Struct for the output JSON file
+type jsonMessage struct {
+    Program     string
+    Version     string
+    Timestamp   time.Time
+    Input       int 
+    Output      string
+}
+
+// Function that takes in an empty interface
+// Allowing for future updates of the API to reuse this code
+func respondJSON (output interface{}, w http.ResponseWriter, r *http.Request) {
+    // Convert Message to JSON
+    jsonResponse, _ := json.MarshalIndent(output, "", "\t")
+    
+    // Indicate to browser file is to be downloaded and serve
+    w.Header().Add("Content-Disposition", "Attachment;filename=response")
+    w.WriteHeader(http.StatusCreated)
+    w.Write(jsonResponse) 
+}
+
+// Home page to act as a help page
+func homePage(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Welcome to the Multiples API\nPlease provide an input value in the form:\n" +
+                   "127.0.0.1/eval/<x> or localhost/eval/<x>\n" +
+                   "Where \"<x>\" is the number you wish to evaluate")
+}
+
+// Error Handling for no input eval value provided
+func noInputValue(w http.ResponseWriter, r *http.Request) {
+    // Generate JSON Structure Message and send via respondJSON
+    createResponse := jsonMessage{Program: "multiplesAPI", 
+                                  Version: "1.0", 
+                                  Timestamp: time.Now().UTC(), 
+                                  Input: 0, 
+                                  Output: "No Input Value Provided"}
+    respondJSON(createResponse, w, r)
+}
+
+// Calculate response to input provided
+func evaluateValue(w http.ResponseWriter, r *http.Request) {
+    // Receive input value to test
+    vars := mux.Vars(r)
+    inputVal, err := strconv.Atoi(vars["id"])
+    if err != nil {
+        fmt.Fprintf(w, "Invalid input, an integer must be provided\n\n" +
+                     "Please provide an input value in the form:\n" +
+                     "127.0.0.1/eval/<x> or localhost/eval/<x>\n" +
+                     "Where \"<x>\" is the number you wish to evaluate")
+        return
+    }
+    
+    // Calculate Response
+    var response string = ""  
+    if (inputVal % 7) == 0 { 
+        response += "C"                   
+    }    
+    if (inputVal % 9) == 0 { 
+        response += "N"                   
+    }
+    if response == ""      {
+        response = strconv.Itoa(inputVal) 
+    } 
+    
+    // Generate JSON Structure Message and send via respondJSON
+    createResponse := jsonMessage{Program: "multiplesAPI", 
+                                  Version: "1.0", 
+                                  Timestamp: time.Now().UTC(), 
+                                  Input: inputVal, 
+                                  Output: response}
+    respondJSON(createResponse, w, r)
+}
+
+// Initialise request router and handler for each expected input path
+func handleRequests() {
+    myRouter := mux.NewRouter().StrictSlash(true)
+    myRouter.HandleFunc("/", homePage)
+    myRouter.HandleFunc("/GET/", noInputValue)
+    myRouter.HandleFunc("/GET/{id}", evaluateValue)
+    // Create server to listen on port 80
+    http.ListenAndServe(":80", myRouter)
+}
+
+// Main function to call handle requests
+func main() {
+    handleRequests()
+}
